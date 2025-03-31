4636a914648858f438f92393a49959dc3ac7302b
Randolph Settgast <rrsettgast@gmail.com>
2025-03-31 18:13:50 +0900

    device tests for shiva B

--- src/finiteElement/unitTests/testSEMQkGLIntegralsShiva.cpp
+++ src/finiteElement/unitTests/testSEMQkGLIntegralsShiva.cpp
@@ -8,6 +8,55 @@ using namespace shiva::functions;
 using namespace shiva::geometry;
 using namespace shiva::discretizations::finiteElementMethod;
 
+void setX( double (&X)[8][3] )
+{
+  double x0=0,y0=0,z0=0;
+  double x1=20,y1=20,z1=20;
+
+  double X0[8][3] = { {x0,y0,z0},
+                     {x1,y0,z0},
+                     {x0,y1,z0},
+                     {x1,y1,z0},
+                     {x0,y0,z1},
+                     {x1,y0,z1},
+                     {x0,y1,z1},
+                     {x1,y1,z1} }; 
+  
+  for( int a=0; a<8; ++a )
+  {
+    for( int i=0; i<3; ++i )
+    {
+      X[a][i] *= X0[a][i] * ( 0.9 + 0.1 * (rand() % 20) );
+    }
+  }
+
+}
+
+
+
+template< typename INTEGRALS >
+void runtimeCalcBKernel( double const (&X)[8][3], 
+                         double (&B)[6] ) 
+{
+  using Integrals = INTEGRALS;
+  using TransformType = typename INTEGRALS::TransformType;
+
+  TransformType trilinearCell;
+  trilinearCell.setData( X );
+
+  pmpl::genericKernelWrapper( 6, B, [trilinearCell] SHIVA_DEVICE ( auto * device_data )
+  {
+    double B[6] = {0.0};
+    Integrals::template computeB<0,0,0>( trilinearCell, B );
+    device_data[0] = B[0];
+    device_data[1] = B[1];
+    device_data[2] = B[2];
+    device_data[3] = B[3];
+    device_data[4] = B[4];
+    device_data[5] = B[5];
+  } );
+}
+
 TEST( testIntegrals, test_computeStiffnessTerm )
 {
   using TransformType =
@@ -28,18 +77,11 @@ TEST( testIntegrals, test_computeStiffnessTerm )
   using Integrals = SEMQkGLIntegralsShiva< double, 1, TransformType, ParentElementType >;
   
 
-  double x0=0,y0=0,z0=0;
-  double x1=20,y1=20,z1=20;
-  double X[8][3]={{x0,y0,z0},{x1,y0,z0},{x0,y1,z0},{x1,y1,z0},
-                  {x0,y0,z1},{x1,y0,z1},{x0,y1,z1},{x1,y1,z1}}; 
+  double X[8][3]; 
+  double B[6] = {0};
+  setX(X);
 
-
-  TransformType trilinearCell;
-  trilinearCell.setData( X );
-  double B[6] = {0.0};
-
-  Integrals::computeB<0,0,0>( trilinearCell, B );
-
+  runtimeCalcBKernel<Integrals>( X, B );
 }
 
 
