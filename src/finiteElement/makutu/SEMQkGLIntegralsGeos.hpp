d244f1ded12b0051eeffca6a7010cddc2b2de2c4
aurelien <aurelien.citrain@inria.fr>
2025-09-30 18:18:56 +0200

    Continu renaming + remove unused files

--- src/finiteElement/makutu/SEMQkGLIntegralsGeos.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef SEMQKINTEGRALSGEOS_HPP_
-#define SEMQKINTEGRALSGEOS_HPP_
-
-#include <sem_data.h>
-#include <sem_macros.h>
-#include <dataType.hpp>
-#include "Qk_Hexahedron_Lagrange_GaussLobatto.hpp"
-#endif // SEMQKINTEGRALSGEOS_HPP_
